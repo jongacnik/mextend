04d5f3523b976b23ed77cce7d6e311b991d51453
amongiants <jon@folderstudio.com>
2015-02-14 01:08:17 -0800

    don't deep merge arrays stupid, just objects

--- mextend.js
+++ mextend.js
@@ -1,7 +1,7 @@
 var mextend = function(target, source){
   target = target || {};
   for (var prop in source) {
-    if (typeof source[prop] === 'object') {
+    if (typeof source[prop] === 'object' && !source[prop] instanceof Array) {
       target[prop] = mextend(target[prop], source[prop]);
     } else {
       target[prop] = source[prop];
